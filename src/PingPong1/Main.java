9e83946f875a7c8f7ccc65edbd911034e4bed0f0
Gani <ganiiz@mail.ru>
2021-07-21 17:10:38 +0600

    Test

--- src/PingPong1/Main.java
+++ src/PingPong1/Main.java
@@ -7,6 +7,7 @@ public class Main {
         AtomicBoolean flag = new AtomicBoolean(false);
         new Thread(()-> new Ping(flag).game()).start();
         new Thread(()-> new Pong(flag).game()).start();
+
     }
 
     static class Ping{
